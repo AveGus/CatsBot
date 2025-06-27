c441f302f7c789abfdfd51a89514b8f6678aeee7
Eldar Garipov <70773701+AveGus@users.noreply.github.com>
2025-06-27 23:37:06 +0500

    A attachment to Review #1 (#5)
    * Liquibase
    
    * Fix kafka WO ZooKeeper, add postgres
    
    * Bot schema and users table
    
    * Tg dependency
    
    * A high-level carcass of interfaces and some concrete classes
    
    * Commit common classes that may be described via interface in code review
    
    * Save changes
    
    * Refactoring

--- TelegramConnector/src/main/resources/db/changelog/1.0/2025-06-24--users-table.sql
+++ TelegramConnector/src/main/resources/db/changelog/1.0/2025-06-24--users-table.sql
@@ -1,10 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS bot;
 
-DROP TABLE IF EXISTS bot."Users";
+DROP TABLE IF EXISTS bot."users";
 
-CREATE TABLE IF NOT EXISTS bot."Users"
+CREATE TABLE IF NOT EXISTS bot."users"
 (
-    user_id         VARCHAR(255) PRIMARY KEY NOT NULL,
-    state           VARCHAR(255)             NOT NULL,
-    created         TIMESTAMPTZ              NOT NULL
+    id                  BIGINT       PRIMARY KEY NOT NULL,
+    username            VARCHAR(255)             NOT NULL,
+    state               VARCHAR(255)             NOT NULL,
+    created             TIMESTAMPTZ              NOT NULL
 );
